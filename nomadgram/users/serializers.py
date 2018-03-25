0d112ba6d35d499c2f2a1bdbb2d07a3b063835c0
tom <ssisksl77@gmail.com>
2018-03-25 10:34:17 +0900

    add image details on user profile

--- nomadgram/users/serializers.py
+++ nomadgram/users/serializers.py
@@ -1,5 +1,26 @@
 from rest_framework import serializers
 from . import models
+from nomadgram.images import serializers as images_serializers
+
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    images = images_serializers.UserProfileImageSerializer(many=True)
+
+    class Meta:
+        model = models.User
+        fields = (
+            'username',
+            'name',
+            'bio',
+            'website',
+            # model property에서 생성
+            'post_count',
+            'followers_count',
+            'following_count',
+            'images',
+        )        
 
 
 class ExploreUserSerializer(serializers.ModelSerializer):
